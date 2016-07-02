9ed997d407d44e528cfac05fa1f874b005d9728e
Luis Gamero <gamero.luis.enrique@gmail.com>
2016-07-01 18:25:40 -0700

    add basic login text inputs and buttons

--- mobile_client/www/components/login/loginController.js
+++ mobile_client/www/components/login/loginController.js
@@ -2,6 +2,7 @@ angular.module('app.LoginController', [])
 
 .controller('LoginController', LoginController);
 
-function LoginController($scope) {
+function LoginController($scope, $state) {
   $scope.data = {};
+  $scope.login = function(){};
 }
