b147c3944cf1a7152af32381b9a2af29f015aea5
Daniel <dela1000@gmail.com>
2016-07-09 21:05:26 -0700

    Add initial map display to event template

--- mobile_client/www/components/event/eventController.js
+++ mobile_client/www/components/event/eventController.js
@@ -1,20 +1,22 @@
 angular.module('app.EventController', [])
-.controller('EventController', function($scope, $state, $rootScope, $ionicPopup, EventFactory){
-
+.controller('EventController', function($scope, $state, $rootScope, $ionicPopup, $ionicLoading, $compile, EventFactory){
   //All data for current event
   $scope.currentEventInView = $rootScope.currentEvent.event
 
+  console.log("$scope.currentEventInView: ", JSON.stringify($scope.currentEventInView, null, "\t"));
+
   //Attendees of current event
-  $scope.currentEventAttendees = $rootScope.currentEvent.attendees;
+  $scope.currentEventAttendees = _.orderBy($rootScope.currentEvent.attendees, ['username'],['asc'])
+  // $rootScope.currentEvent.attendees
+  // $scope.currentEventAttendees = $rootScope.currentEvent.attendees;
+
+  $scope.eventLoading = false;
 
   //User is attending this event
   $scope.userAttending = false;
 
-  console.log("+++ 7 eventController.js $scope.currentEventInView: ", $scope.currentEventInView)
-
   var isUserGoing = function () {
     _.forEach($scope.currentEventAttendees, function (attendee) {
-      console.log("+++ 11 eventController.js attendee: ", attendee)
       if(attendee.userId === $rootScope.userId){
         $scope.userAttending = true;
       }
@@ -24,7 +26,65 @@ angular.module('app.EventController', [])
     //   console.log("+++ 24 eventController.js locationResult: ", locationResult)
     // })
   }
+  function drawMap() {
+    console.log("+++ 30 eventController.js $scope.currentEventInView.ownerLat, $scope.currentEventInView.ownerLong: ", $scope.currentEventInView.ownerLat, $scope.currentEventInView.ownerLong)
+    if($scope.currentEventInView.centerLat === null || $scope.currentEventInView.centerLat === undefined){
+       var myLatlng = new google.maps.LatLng($scope.currentEventInView.ownerLat, $scope.currentEventInView.ownerLong);
+    }else{
+       var myLatlng = new google.maps.LatLng($scope.currentEventInView.centerLat, $scope.currentEventInView.centerLong);
+    }
 
+       var mapOptions = {
+         center: myLatlng,
+         zoom: 16,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+       };
+       var map = new google.maps.Map(document.getElementById("map"),
+           mapOptions);
+
+       //Marker + infowindow + angularjs compiled ng-click
+       var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+       var compiled = $compile(contentString)($scope);
+
+       var infowindow = new google.maps.InfoWindow({
+         content: compiled[0]
+       });
+
+       var marker = new google.maps.Marker({
+         position: myLatlng,
+         map: map,
+         title: 'Uluru (Ayers Rock)'
+       });
+
+       google.maps.event.addListener(marker, 'click', function() {
+         infowindow.open(map,marker);
+       });
+
+       $scope.map = map;
+     }
+     google.maps.event.addDomListener(window, 'load', drawMap);
+
+     $scope.centerOnMe = function() {
+       if(!$scope.map) {
+         return;
+       }
+
+       $scope.loading = $ionicLoading.show({
+         content: 'Getting current location...',
+         showBackdrop: false
+       });
+
+       navigator.geolocation.getCurrentPosition(function(pos) {
+         $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+         $scope.loading.hide();
+       }, function(error) {
+         alert('Unable to get location: ' + error.message);
+       });
+     };
+
+     $scope.clickTest = function() {
+       alert('Example of infowindow with ng-click')
+     };
 
   //Accept Event
   $scope.acceptEvent = function(eventId) {
@@ -38,7 +98,12 @@ angular.module('app.EventController', [])
 
     // })
   }
+  var initialize = function () {
+    isUserGoing();
+    drawMap();
+    // eventLoading();
+  };
 
-  isUserGoing();
+  initialize();
 
 })
